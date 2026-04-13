1d760b8b4ad27a1725f4a0c8f96174cbedfd5a5c
tumelots07 <st10452101@rcconnect.edu.za>
2026-04-13 04:21:50 +0200

    Connected registration and login in main application

--- QuickChatApp/src/main/java/com/mychatbot/quickchatapp/QuickChatApp.java
+++ QuickChatApp/src/main/java/com/mychatbot/quickchatapp/QuickChatApp.java
@@ -1,16 +1,36 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mychatbot.quickchatapp;
 
-/**
- *
- * @author ttsim
- */
+import java.util.Scanner;
+
 public class QuickChatApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Scanner input = new Scanner(System.in);
+        Login login = new Login();
+
+        System.out.println("===== QUICKCHAT APP =====");
+
+        // REGISTER
+        System.out.print("Enter username: ");
+        String username = input.nextLine();
+
+        System.out.print("Enter password: ");
+        String password = input.nextLine();
+
+        System.out.println(login.registerUser(username, password));
+
+        // LOGIN
+        System.out.println("\n=== LOGIN ===");
+
+        System.out.print("Enter username: ");
+        String loginUser = input.nextLine();
+
+        System.out.print("Enter password: ");
+        String loginPass = input.nextLine();
+
+        boolean loggedIn = login.loginUser(loginUser, loginPass);
+
+        System.out.println(login.returnLoginStatus(loggedIn, loginUser));
     }
 }
