bed00a15838ebb700d713b5d664b72cce7c11c23
tumelots07 <st10452101@rcconnect.edu.za>
2026-04-13 04:05:14 +0200

    Added password complexity validation

--- QuickChatApp/src/main/java/com/mychatbot/quickchatapp/Login.java
+++ QuickChatApp/src/main/java/com/mychatbot/quickchatapp/Login.java
@@ -3,7 +3,22 @@ package com.mychatbot.quickchatapp;
 public class Login {
 
     public boolean checkUserName(String username) {
-        // must contain underscore and be <= 5 characters
         return username.contains("_") && username.length() <= 5;
+    }
+
+    public boolean checkPasswordComplexity(String password) {
+        // At least 8 characters
+        boolean length = password.length() >= 8;
+
+        // Contains capital letter
+        boolean capital = password.matches(".*[A-Z].*");
+
+        // Contains number
+        boolean number = password.matches(".*[0-9].*");
+
+        // Contains special character
+        boolean special = password.matches(".*[^a-zA-Z0-9].*");
+
+        return length && capital && number && special;
     }
 }
