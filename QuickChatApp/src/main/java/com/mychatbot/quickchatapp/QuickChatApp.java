3ec41ec7c3fa89afb412e7d5000c31bd6dfdb4b5
tumelots07 <st10452101@rcconnect.edu.za>
2026-04-13 04:00:59 +0200

    Initial project setup (NetBeans Maven project)

---
+++ QuickChatApp/src/main/java/com/mychatbot/quickchatapp/QuickChatApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mychatbot.quickchatapp;
+
+/**
+ *
+ * @author ttsim
+ */
+public class QuickChatApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
